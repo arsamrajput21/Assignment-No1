c2b58230e267a2e7de6c187231bf9134b32d7a1f
arsamrajput21 <46869096+arsamrajput21@users.noreply.github.com>
2019-01-24 10:10:36 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,212 @@
+#include <stdio.h>
+
+//int main(void)
+//{
+    //printf("hello world\n");
+    //return 0;
+//} 
+
+//FIRST PROGRAM.........
+
+//int main()
+//{
+//int firstnum, secondnum, sumofnums;
+
+//printf("Enter numbers");
+
+//scanf("%d  %d ",&firstnum, &secondnum);
+
+//sumofnums = firstnum + secondnum;
+
+//printf("%d + %d = %d",firstnum , secondnum , sumofnums);
+
+//return 0;
+
+//}
+
+
+
+// SECOND PROGRAM........
+
+
+//int main()
+//{
+//int firstnum, secondnum, sumofnums;
+
+//printf("Enter numbers for negative");
+
+//scanf("%d  %d ",&firstnum, &secondnum);
+
+//sumofnums = firstnum - secondnum;
+
+//printf("%d - %d = %d",firstnum , secondnum , sumofnums);
+
+//return 0;
+
+//}
+
+
+
+// third program......
+
+
+//int main()
+//{
+    //int firstnum, secondnum, sumofnums;
+
+//printf("Enter numbers for multiply");
+
+//scanf("%d  %d ",&firstnum, &secondnum);
+
+//sumofnums = firstnum * secondnum;
+
+//printf("%d * %d = %d",firstnum , secondnum , sumofnums);
+
+//return 0;
+//}
+
+
+//fourth program largest number  .....
+
+//int main()
+//{
+    //double n1 , n2 , n3;
+    //printf("Enter three different numbers :");
+    //scanf("%lf %lf %lf" , &n1 , &n2 , &n3);
+    
+    //if(n1>=n2 && n1>=n3)
+        //printf("%.2f is the largest number.",n1);
+    
+    //if(n2>=n1 && n2>=n3)
+        //printf("%.2f is the largest number.",n2);
+    
+    //if(n3>=n1 && n3>=n2)
+        //printf("%.2f is the largest number.",n3);
+    
+    //return 0;
+//}
+
+
+
+
+//// fifth program number is possitive or negative 
+
+//int main()
+//{
+    //double number;
+    //printf("Enter a number");
+    //scanf("%lf",&number);
+    
+    //if(number <= 0.0)
+    //{
+        //if(number == 0.0)
+            //printf("You Entered 0.");
+        //else 
+            //printf("You Entered a negative number.");
+    //}
+    //else 
+        //printf("You Entered  a possitive number.");
+    //return 0 ;
+//}
+
+
+
+//sixth program number is alphabet or not?
+
+//int main()
+//{
+    //char c;
+    //printf("Entered a character : ");
+    //scanf("%c",&c);
+    
+    //if((c>='a' && c<='z') || (c>='A' && c<='Z'))
+        //printf("%c is an alphabet. ",c);
+    //else
+        //printf("%c is not an alphabet . ",c);
+    
+    //return 0;
+//}
+
+
+
+
+
+//seventh program natural numbers sum 
+
+//int main ()
+//{
+    //int n,i, sum = 0;
+    
+    //printf("Entered a sum of natural numbers: ");
+    //scanf("%d",&n);
+    
+    //for(i= 1; i<= n; ++i)
+    //{
+        //sum +=i;
+    //}
+    //printf("Sum = %d",sum);
+    //return 0;
+//}
+
+
+
+// eight program number is even or odd
+
+//int main()
+//{
+    //int number;
+    //printf("Entered an integer: ");
+    //scanf("%d",&number);
+    
+    //if(number % 2 == 0)
+        //printf("%d is even . ",number);
+    //else
+        //printf("%d is odd . ",number);
+    
+    //return 0 ;
+//}
+
+
+
+ 
+//ninth program count number of digits in an ineteger
+
+//int main()
+//{
+    //long long n;
+    //int count = 0;
+    
+    //printf("Entred an integer: ");
+    //scanf("%lld", &n);
+    
+    //while(n != 0)
+    //{
+        //n /= 10;
+        //++count;
+    //}
+    //printf("Number of digits:  %d",count);
+//}
+
+
+
+
+
+//tenth program factors of numbers
+
+//int main()
+//{
+    //int number, i;
+    
+    //printf("Enetered a positive integer: ");
+    //scanf("%d",&number);
+    
+    //printf("Factors of %d are: ",number);
+    //for(i =1; i<=number; ++i)
+    //{
+        //if(number%i == 0)
+        //{
+            //printf("%d ",i);
+        //}
+    //}
+    //return 0;
+//}
